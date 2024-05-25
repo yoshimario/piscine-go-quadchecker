cd92c0a418db6fc4dd1c8ed12295d9585f8b516e
Allen Lee Yuan Neng <leeyn.shun@gmail.com>
2024-05-25 23:27:29 +0300

    re:quadS

--- quadD.go
+++ quadD.go
@@ -1,6 +1,34 @@
-package piscine
+package main
 
-import "github.com/01-edu/z01"
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	args := os.Args[1:]
+	argsLen := len(args)
+
+	if argsLen != 2 {
+		return
+	}
+
+	col := atoi(args[0])
+	row := atoi(args[1])
+	QuadD(row, col)
+	os.Exit(0)
+}
+
+func atoi(s string) int {
+	num := 0
+	sLen := len(s)
+
+	for i := 0; i < sLen; i++ {
+		num = num*10 + int(s[i]-48)
+	}
+	return num
+}
 
 func QuadD(x, y int) {
 	if x > 0 && y > 0 {
