34fb179bff218ffcc57051012967b36df7e4fb3e
Allen Lee Yuan Neng <leeyn.shun@gmail.com>
2024-05-25 21:44:40 +0300

    checking your quads

--- main.go
+++ main.go
@@ -1,119 +1,135 @@
-package piscine
+package main
 
-import "github.com/01-edu/z01"
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"os"
+)
 
-func QuadA(x, y int) {
-	if x <= 0 || y <= 0 {
-		return // if number is negative, it will exit and not execute
-	}
-	for row := 1; row <= y; row++ {
-		for col := 1; col <= x; col++ {
-			if (row == 1 && col == 1) || (row == 1 && col == x) || (row == y && col == 1) || (row == y && col == x) {
-				z01.PrintRune('o')
-			} else if row == 1 || row == y {
-				z01.PrintRune('-')
-			} else if col == 1 || col == x {
-				z01.PrintRune('|')
-			} else {
-				z01.PrintRune(' ')
+func main() {
+	var buffer bytes.Buffer
+	tempBuf := make([]byte, 1024)
+	for {
+		count, err := os.Stdin.Read(tempBuf)
+		if err != nil {
+			if err == io.EOF {
+				break
 			}
+			os.Exit(1)
 		}
-		z01.PrintRune('\n')
+		buffer.Write(tempBuf[:count])
+
 	}
+
+	inputB := buffer.Bytes()
+
+	/* inputStat, _ := os.Stdin.Stat()          // grab info of output of previous command
+	inputB := make([]byte, inputStat.Size()) // create array of correct size for output of previous command
+	os.Stdin.Read(inputB)                    // grab output into array inputB */
+
+	inputS := string(inputB) // convert input byte to input string
+	inputSLen := len(inputB) // get length of input
+
+	// check the number of rows and col
+	col := 0
+	row := 0
+	for i := 0; i < inputSLen; i++ {
+		if inputS[i] != '\n' {
+			col++
+		} else {
+			break
+		}
+	}
+
+	for i := 0; i < inputSLen; i++ {
+		if inputS[i] == '\n' {
+			row++
+		}
+	}
+
+	// generate quadABCDE and compare with input to see if it matches any
+	result := "" // empty string to store result
+	numRes := 0  // count number of results
+	if inputS == prntBox(col, row, 'o', 'o', 'o', 'o', '-', '|') {
+		result = "[quadA] [" + itoa(col) + "] [" + itoa(row) + "]"
+		numRes++
+	}
+	if inputS == prntBox(col, row, '/', '\\', '\\', '/', '*', '*') {
+		result = "[quadB] [" + itoa(col) + "] [" + itoa(row) + "]"
+		numRes++
+	}
+	if inputS == prntBox(col, row, 'A', 'A', 'C', 'C', 'B', 'B') {
+		result = "[quadC] [" + itoa(col) + "] [" + itoa(row) + "]"
+		numRes++
+	}
+	if inputS == prntBox(col, row, 'A', 'C', 'A', 'C', 'B', 'B') {
+		if numRes == 0 {
+			result = "[quadD] [" + itoa(col) + "] [" + itoa(row) + "]"
+		} else {
+			result = result + " || " + "[quadD] [" + itoa(col) + "] [" + itoa(row) + "]"
+		}
+		numRes++
+	}
+	if inputS == prntBox(col, row, 'A', 'C', 'C', 'A', 'B', 'B') {
+		if numRes == 0 {
+			result = "[quadE] [" + itoa(col) + "] [" + itoa(row) + "]"
+		} else {
+			result = result + " || " + "[quadE] [" + itoa(col) + "] [" + itoa(row) + "]"
+		}
+		numRes++
+	}
+
+	if numRes == 0 {
+		fmt.Println("Not a quad function")
+	} else {
+		fmt.Println(result)
+	}
+	os.Exit(0)
 }
 
-func QuadB(x, y int) {
-	if x > 0 && y > 0 {
+func itoa(num int) string {
+	s := ""
 
-		for row := 1; row <= y; row++ {
-			for col := 1; col <= x; col++ {
-				if row == 1 && col == 1 {
-					z01.PrintRune('/')
-				} else if (row == 1 && col == x) || (row == y && col == 1) {
-					z01.PrintRune('\\')
-				} else if row == y && col == x {
-					z01.PrintRune('/')
-				} else if (row == 1 || row == y) && (col > 1 && col < x) {
-					z01.PrintRune('*')
-				} else if (col == 1 || col == x) && (row > 1 && row < y) {
-					z01.PrintRune('*')
-				} else {
-					z01.PrintRune(' ')
-				}
-			}
-			z01.PrintRune('\n')
-		}
+	if num == 0 {
+		return "0"
 	}
+	for num > 0 {
+		s = string(rune(num%10+48)) + s
+		num = num / 10
+	}
+
+	return s
 }
 
-func QuadC(x, y int) {
-	if x > 0 && y > 0 {
+func prntBox(x, y int, c1, c2, c3, c4, top, side rune) string {
+	s := ""
 
-		for row := 1; row <= y; row++ {
-			for col := 1; col <= x; col++ {
-				if row == 1 && col == 1 {
-					z01.PrintRune('A')
-				} else if row == 1 && col == x {
-					z01.PrintRune('A')
-				} else if row == y && col == x || (row == y && col == 1) {
-					z01.PrintRune('C')
-				} else if (row == 1 || row == y) && (col > 1 && col < x) {
-					z01.PrintRune('B')
-				} else if (col == 1 || col == x) && (row > 1 && row < y) {
-					z01.PrintRune('B')
-				} else {
-					z01.PrintRune(' ')
-				}
-			}
-			z01.PrintRune('\n')
+	for i := 0; i < y; i++ {
+		if i == 0 {
+			s = s + prntLine(x, c1, c2, top)
+		} else if i == y-1 {
+			s = s + prntLine(x, c3, c4, top)
+		} else {
+			s = s + prntLine(x, side, side, ' ')
 		}
 	}
+
+	return s
 }
 
-func QuadD(x, y int) {
-	if x > 0 && y > 0 {
-
-		for row := 1; row <= y; row++ {
-			for col := 1; col <= x; col++ {
-				if (row == 1 && col == 1) || (row == y && col == 1) {
-					z01.PrintRune('A')
-				} else if (row == 1 && col == x) || (row == y && col == x) {
-					z01.PrintRune('C')
-				} else if (row == 1 || row == y) && (col > 1 && col < x) {
-					z01.PrintRune('B')
-				} else if (col == 1 || col == x) && (row > 1 && row < y) {
-					z01.PrintRune('B')
-				} else {
-					z01.PrintRune(' ')
-				}
-			}
-			z01.PrintRune('\n')
+func prntLine(x int, c1 rune, c2 rune, mid rune) string {
+	s := ""
+	for i := 0; i < x; i++ {
+		if i == 0 {
+			s = s + string(c1)
+		} else if i == x-1 {
+			s = s + string(c2)
+		} else {
+			s = s + string(mid)
 		}
 	}
-}
+	s = s + string('\n')
 
-func QuadE(x, y int) {
-	if x > 0 && y > 0 {
-
-		for row := 1; row <= y; row++ {
-			for col := 1; col <= x; col++ {
-				if row == 1 && col == 1 {
-					z01.PrintRune('A')
-				} else if row == 1 && col == x {
-					z01.PrintRune('C')
-				} else if row == y && col == 1 {
-					z01.PrintRune('C')
-				} else if row == y && col == x {
-					z01.PrintRune('A')
-				} else if (row == 1 || row == y) && (col > 1 && col < x) {
-					z01.PrintRune('B')
-				} else if (col == 1 || col == x) && (row > 1 && row < y) {
-					z01.PrintRune('B')
-				} else {
-					z01.PrintRune(' ')
-				}
-			}
-			z01.PrintRune('\n')
-		}
-	}
+	return s
 }
