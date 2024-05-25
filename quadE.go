82b8fe3582b0d9b7b00be59a2ac108062dff3ee4
Sean Kipinä <paintballsean@gmail.com>
2024-05-25 20:29:09 +0300

    added new quad files

--- quadE.go
+++ quadE.go
@@ -1,61 +1,29 @@
-package main
+package piscine
 
-import (
-	"os"
+import "github.com/01-edu/z01"
 
-	"github.com/01-edu/z01"
-)
+func QuadE(x, y int) {
+	if x > 0 && y > 0 {
 
-func main() {
-	args := os.Args[1:]
-	argsLen := len(args)
-
-	if argsLen != 2 {
-		return
-	}
-
-	col := atoi(args[0])
-	row := atoi(args[1])
-	printBox(col, row, 'A', 'C', 'C', 'A', 'B', 'B')
-}
-
-func atoi(s string) int {
-	num := 0
-	sLen := len(s)
-
-	for i := 0; i < sLen; i++ {
-		num = num*10 + int(s[i]-48)
-	}
-	return num
-}
-
-func printBox(x, y int, c1, c2, c3, c4, top, side rune) {
-
-	if x < 1 || y < 1 {
-		return
-	}
-
-	for i := 0; i < y; i++ {
-		if i == 0 {
-			printLine(x, c1, c2, top)
-		} else if i == y-1 {
-			printLine(x, c3, c4, top)
-		} else {
-			printLine(x, side, side, ' ')
+		for row := 1; row <= y; row++ {
+			for col := 1; col <= x; col++ {
+				if row == 1 && col == 1 {
+					z01.PrintRune('A')
+				} else if row == 1 && col == x {
+					z01.PrintRune('C')
+				} else if row == y && col == 1 {
+					z01.PrintRune('C')
+				} else if row == y && col == x {
+					z01.PrintRune('A')
+				} else if (row == 1 || row == y) && (col > 1 && col < x) {
+					z01.PrintRune('B')
+				} else if (col == 1 || col == x) && (row > 1 && row < y) {
+					z01.PrintRune('B')
+				} else {
+					z01.PrintRune(' ')
+				}
+			}
+			z01.PrintRune('\n')
 		}
 	}
-	z01.PrintRune('\n')
-}
-
-func printLine(x int, c1 rune, c2 rune, mid rune) {
-	for i := 0; i < x; i++ {
-		if i == 0 {
-			z01.PrintRune(c1)
-		} else if i == x-1 {
-			z01.PrintRune(c2)
-		} else {
-			z01.PrintRune(mid)
-		}
-	}
-	z01.PrintRune('\n')
 }
